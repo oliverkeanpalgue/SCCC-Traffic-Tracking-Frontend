ac353b8fbaa2673cf3e5e6f6033a5ebfcac134b0
Jan Ryan Ballasio <98378424+JanBallasio@users.noreply.github.com>
2025-05-08 13:25:24 +0800

    feat/moved-coordinates-to-db- added-map-style-option

--- src/components/TrafficTraficking/TrafficLevelModal.vue
+++ src/components/TrafficTraficking/TrafficLevelModal.vue
@@ -1,19 +1,14 @@
 <template>
-  <div 
-    v-if="activeRoad"
+  <div v-if="activeRoad"
     class="fixed text-white w-[500px] bg-[#1b1a1a] z-[1000] top-[24px] left-[930px] -translate-x-1/2 p-5 rounded-xl"
-    @click.stop
-  >
+    @click.stop>
     <div class="flex flex-col gap-4">
       <div class="relative">
         <!-- Road Name -->
         <div class="text-center">{{ activeRoad.properties.name }}</div>
 
         <!-- Close Button -->
-        <CdChromeClose 
-          class="absolute top-1 right-2 text-white cursor-pointer" 
-          @click.stop="closeEditModal" 
-        />
+        <CdChromeClose class="absolute top-1 right-2 text-white cursor-pointer" @click.stop="closeEditModal" />
       </div>
 
       <div class="flex">
@@ -22,17 +17,17 @@
           <div class="flex gap-4 items-center">
             <h1>Inbound</h1>
             <div class="flex items-center cursor-pointer relative" @click.stop="toggleDropdown('inbound')">
-              <div :style="{ backgroundColor: colorMap[activeRoad.inboundColor] }"
-                class="w-[20px] h-[20px] rounded-xs"></div>
+              <div :style="{ backgroundColor: colorMap[activeRoad.inboundColor] }" class="w-[20px] h-[20px] rounded-xs">
+              </div>
 
               <!-- Dropdown Menu -->
               <div v-if="activeDropdown === 'inbound'"
                 class="absolute mt-2 bg-[#1b1a1a] p-2 rounded-lg top-full left-0 z-[1001]">
-                <div v-for="color in ['green', 'yellow', 'red']" :key="color"
+                <div v-for="(label, color) in trafficLabels" :key="color"
                   class="p-2 cursor-pointer flex items-center gap-2"
                   @click.stop="changeTrafficLevel(activeRoad.properties.id, 'inbound', color)">
                   <div class="w-[20px] h-[20px] rounded-xs" :style="{ backgroundColor: colorMap[color] }"></div>
-                  <span class="text-white">{{ color.charAt(0).toUpperCase() + color.slice(1) }}</span>
+                  <span class="text-white">{{ label }}</span>
                 </div>
               </div>
             </div>
@@ -50,11 +45,11 @@
               <!-- Dropdown Menu -->
               <div v-if="activeDropdown === 'outbound'"
                 class="absolute mt-2 bg-[#1b1a1a] p-2 rounded-lg top-full left-0 z-[1001]">
-                <div v-for="color in ['green', 'yellow', 'red']" :key="color"
+                <div v-for="(label, color) in trafficLabels" :key="color"
                   class="p-2 cursor-pointer flex items-center gap-2"
                   @click.stop="changeTrafficLevel(activeRoad.properties.id, 'outbound', color)">
                   <div class="w-[20px] h-[20px] rounded-xs" :style="{ backgroundColor: colorMap[color] }"></div>
-                  <span class="text-white">{{ color.charAt(0).toUpperCase() + color.slice(1) }}</span>
+                  <span class="text-white">{{ label }}</span>
                 </div>
               </div>
             </div>
@@ -77,7 +72,11 @@ const props = defineProps({
 const emit = defineEmits(["changeTrafficLevel", "closeEditModal"]);
 
 const activeDropdown = ref(null);
-
+const trafficLabels = {
+  'green': 'Light',
+  'yellow': 'Moderate',
+  'red': 'Heavy'
+};
 const toggleDropdown = (direction) => {
   activeDropdown.value = activeDropdown.value === direction ? null : direction;
 };
