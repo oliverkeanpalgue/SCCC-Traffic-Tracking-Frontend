39addf60630745913fceb6e96b4e820d9c9b977f
oliverkeanpalgue <106715785+oliverkeanpalgue@users.noreply.github.com>
2025-05-24 20:38:21 +0800

    feat: revised register

--- src/components/TrafficTraficking/Navbar.vue
+++ src/components/TrafficTraficking/Navbar.vue
@@ -51,7 +51,7 @@
                                 class="text-white hover:text-underline hover:text-green-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 focus:outline-none">
                                 Login
                             </a>
-                            <a href="/register"
+                            <a href="/signup"
                                 class="text-white hover:text-underline hover:text-green-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 focus:outline-none">
                                 Register
                             </a>
