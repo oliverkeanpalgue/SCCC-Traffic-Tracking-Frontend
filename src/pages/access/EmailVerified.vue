49728198fb2c498970f680254540d9bd032873ee
oliverkeanpalgue <106715785+oliverkeanpalgue@users.noreply.github.com>
2025-05-25 14:44:48 +0800

    refactor: revised email is verified page

--- src/pages/access/EmailVerified.vue
+++ src/pages/access/EmailVerified.vue
@@ -1,7 +1,5 @@
 <script setup>
 import { useRouter } from 'vue-router';
-import errorBg from '../../assets/darkBlueBg.jpg';
-import { MdMarkEmailRead } from '@kalimahapps/vue-icons';
 import { IoOutlineArrowForward } from '@kalimahapps/vue-icons';
 
 const router = useRouter();
@@ -12,25 +10,16 @@ const goHome = () => {
 </script>
 
 <template>
-    <div :style="{
-        backgroundImage: `url(${errorBg})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-    }" class="flex flex-col items-center justify-center min-h-screen w-full">
+    <div class="bg-dark flex flex-col items-center justify-center min-h-screen w-full relative">
+        <div class="absolute inset-0 bg-black opacity-60 z-0"></div>
 
-        <div class="absolute inset-0 bg-teal-700/30 z-0"></div>
-
-        <div
-            class="animate-rotate-border rounded-3xl p-1.5 transition-all duration-500 ease-out bg-conic/[from_var(--border-angle)] from-green-500/10 via-green-500 to-green-500/10">
-            <div
-                class="bg-black/95 backdrop-blur-lg rounded-3xl p-20 flex flex-col items-center justify-center text-white">
-                <MdMarkEmailRead class="w-50 h-50 mb-6 text-green-700" />
-                <p class="text-5xl font-bold mb-1">EMAIL VERIFIED</p>
-                <p class="text-xl mt-2">Your email has been successfully verified! You can now access the SCCC-Inventory System.
-                </p>
+        <div class="relative z-10 rounded-3xl p-10 bg-black/80 backdrop-blur-md shadow-lg max-w-sm flex items-center justify-center flex-col">
+            <img src="/img/Logo.png" alt="Logo" class="w-24 h-24 mb-6" />
+            <h1 class="text-3xl font-semibold text-green-500 mb-2">Email Verified</h1>
+            <p class="text-white text-center mb-6">Your email has been successfully verified! You can now access the website.</p>
+            <div class="flex flex-col space-y-2">
                 <button @click="goHome"
-                    class="mt-6 px-5 py-2 cursor-pointer rounded-lg bg-green-700 hover:bg-green-700 hover:scale-105 text-white font-medium shadow-md transition-all duration-300 flex items-center gap-2">
+                    class="mt-3 cursor-pointer w-full px-6 py-2 bg-primary hover:bg-primary-100 text-white rounded-md shadow transition transform hover:scale-105 flex items-center justify-center gap-2">
                     Go to Dashboard
                     <IoOutlineArrowForward class="w-5 h-5" />
                 </button>
